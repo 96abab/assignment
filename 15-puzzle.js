a1cfd274934b5f0e754b64fdb8b02d5cd5913c56
96abab <96abab@naver.com>
2022-12-14 19:03:00 +0900

    image추가

--- 15-puzzle.js
+++ 15-puzzle.js
@@ -1,54 +1,43 @@
-/**
- * 15-puzzle.js
- *
- * Copyright (c) 2015 Arnis Ritins
- * Released under the MIT license
- */
-(function(){
+(function() {
 	
 	var state = 1;
 	var puzzle = document.getElementById('puzzle');
 
-	// Creates solved puzzle
 	solve();
 	
-	// Listens for click on puzzle cells
-	puzzle.addEventListener('click', function(e){
-		if(state == 1){
-			// Enables sliding animation
+	puzzle.addEventListener('click', function(e) {
+		if(state == 1) {
 			puzzle.className = 'animate';
 			shiftCell(e.target);
 		}
 	});
 	
-	// Listens for click on control buttons
-	// document.getElementById('solve').addEventListener('click', solve);
+
 	document.getElementById('scramble').addEventListener('click', scramble);
 
-	/**
-	 * Creates solved puzzle
-	 *
-	 */
-	function solve(){
+
+	function solve() {
 		
-		if(state == 0){
+		if(state == 0) {
 			return;
 		}
 		
 		puzzle.innerHTML = '';
 		
 		var n = 1;
-		for(var i = 0; i <= 3; i++){
-			for(var j = 0; j <= 3; j++){
+		for(var i = 0; i <= 3; i++) {
+			for(var j = 0; j <= 3; j++) {
 				var cell = document.createElement('span');
-				cell.id = 'cell-'+i+'-'+j;
+				cell.id = 'cell-'+i+'-'+j;  // +i+ 행을 나타냄
 				cell.style.left = (j*80+1*j+1)+'px';
 				cell.style.top = (i*80+1*i+1)+'px';
 				
-				if(n <= 15){
-					cell.classList.add('number');
-					cell.classList.add((i%2==0 && j%2>0 || i%2>0 && j%2==0) ? 'dark' : 'light');
+				if(n <= 15) {
+					// cell.classList.add("number");
+					cell.classList.add(`number${n}`);
+					cell.classList.add(n);
 					cell.innerHTML = (n++).toString();
+					// n++;
 				} else {
 					cell.className = 'empty';
 				}
@@ -59,30 +48,24 @@
 		
 	}
 
-	/**
-	 * Shifts number cell to the empty cell
-	 * 
-	 */
-	function shiftCell(cell){
+	function shiftCell(cell) {
 		
-		// Checks if selected cell has number
-		if(cell.clasName != 'empty'){
+		if(cell.clasName != 'empty') {
 			
-			// Tries to get empty adjacent cell
 			var emptyCell = getEmptyAdjacentCell(cell);
 			
-			if(emptyCell){
-				// Temporary data
+			if(emptyCell) {
+	
 				var tmp = {style: cell.style.cssText, id: cell.id};
 				
-				// Exchanges id and style values
+
 				cell.style.cssText = emptyCell.style.cssText;
 				cell.id = emptyCell.id;
 				emptyCell.style.cssText = tmp.style;
 				emptyCell.id = tmp.id;
 				
-				if(state == 1){
-					// Checks the order of numbers
+				if(state == 1) {
+
 					setTimeout(checkOrder, 150);
 				}
 			}
@@ -90,62 +73,50 @@
 		
 	}
 
-	/**
-	 * Gets specific cell by row and column
-	 *
-	 */
-	function getCell(row, col){
+
+	function getCell(row, col) {
 	
 		return document.getElementById('cell-'+row+'-'+col);
 		
 	}
 
-	/**
-	 * Gets empty cell
-	 *
-	 */
-	function getEmptyCell(){
+
+	function getEmptyCell() {
 	
 		return puzzle.querySelector('.empty');
 			
 	}
 	
-	/**
-	 * Gets empty adjacent cell if it exists
-	 *
-	 */
-	function getEmptyAdjacentCell(cell){
+
+	function getEmptyAdjacentCell(cell) {
 		
-		// Gets all adjacent cells
+
 		var adjacent = getAdjacentCells(cell);
 		
-		// Searches for empty cell
-		for(var i = 0; i < adjacent.length; i++){
-			if(adjacent[i].className == 'empty'){
+
+		for(var i = 0; i < adjacent.length; i++) {
+			if(adjacent[i].className == 'empty') {
 				return adjacent[i];
 			}
 		}
 		
-		// Empty adjacent cell was not found
+
 		return false;
 		
 	}
 
-	/**
-	 * Gets all adjacent cells
-	 *
-	 */
-	function getAdjacentCells(cell){
+
+	function getAdjacentCells(cell) {
 		
 		var id = cell.id.split('-');
 		
-		// Gets cell position indexes
+
 		var row = parseInt(id[1]);
 		var col = parseInt(id[2]);
 		
 		var adjacent = [];
 		
-		// Gets all possible adjacent cells
+
 		if(row < 3){adjacent.push(getCell(row+1, col));}			
 		if(row > 0){adjacent.push(getCell(row-1, col));}
 		if(col < 3){adjacent.push(getCell(row, col+1));}
@@ -155,43 +126,37 @@
 		
 	}
 	
-	/**
-	 * Chechs if the order of numbers is correct
-	 *
-	 */
-	function checkOrder(){
+
+	function checkOrder() {
 		
-		// Checks if the empty cell is in correct position
-		if(getCell(3, 3).className != 'empty'){
+
+		if(getCell(3, 3).className != 'empty') {
 			return;
 		}
-	
+
 		var n = 1;
-		// Goes through all cells and checks numbers
-		for(var i = 0; i <= 3; i++){
+
+		for(var i = 0; i <= 3; i++) {
 			for(var j = 0; j <= 3; j++){
-				if(n <= 15 && getCell(i, j).innerHTML != n.toString()){
-					// Order is not correct
+				if(n <= 15 && getCell(i, j).innerHTML != n.toString()) {
+
 					return;
 				}
 				n++;
 			}
 		}
 		
-		// Puzzle is solved, offers to scramble it
-		if(confirm('Congrats, You did it! \nScramble the puzzle?')){
+
+		if(confirm('Congrats, You did it! \nScramble the puzzle?')) {
 			scramble();
 		}
 	
 	}
 
-	/**
-	 * Scrambles puzzle
-	 *
-	 */
-	function scramble(){
+
+	function scramble() {
 	
-		if(state == 0){
+		if(state == 0) {
 			return;
 		}
 		
@@ -200,17 +165,17 @@
 		
 		var previousCell;
 		var i = 1;
-		var interval = setInterval(function(){
+		var interval = setInterval(function() {
 			if(i <= 100){
 				var adjacent = getAdjacentCells(getEmptyCell());
 				if(previousCell){
-					for(var j = adjacent.length-1; j >= 0; j--){
-						if(adjacent[j].innerHTML == previousCell.innerHTML){
+					for(var j = adjacent.length-1; j >= 0; j--) {
+						if(adjacent[j].innerHTML == previousCell.innerHTML) {
 							adjacent.splice(j, 1);
 						}
 					}
 				}
-				// Gets random adjacent cell and memorizes it for the next iteration
+
 				previousCell = adjacent[rand(0, adjacent.length-1)];
 				shiftCell(previousCell);
 				i++;
@@ -222,11 +187,8 @@
 
 	}
 	
-	/**
-	 * Generates random number
-	 *
-	 */
-	function rand(from, to){
+
+	function rand(from, to) {
 
 		return Math.floor(Math.random() * (to - from + 1)) + from;
 
